84bfa9cc6e2c5d16fd9c04b6a58637d9f32ddb7c
Toshika Varshney <toshikavarshney08@gmail.com>
2022-05-12 03:39:59 -0700

    Commit

---
+++ OpenCVPrograms/OpenWebcam.py
@@ -0,0 +1,12 @@
+from tkinter import Frame
+import cv2
+
+vid = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = vid.read()
+    cv2.imshow('Frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+vid.release()
+cv2.destroyAllWindows()
