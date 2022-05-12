c782258839b1b7d1732b057408642a0f47282259
Toshika Varshney <toshikavarshney08@gmail.com>
2022-05-11 22:51:32 -0700

    First Commit

---
+++ OpenCVPrograms/DetectPalmLines.py
@@ -0,0 +1,12 @@
+import cv2
+image = cv2.imread("palm.jpg")
+cv2.imshow("palm",image)
+cv2.waitKey(0)
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray,40,55,apertureSize=3)
+cv2.imshow("edges in palm",edges)
+cv2.waitKey(0)
+edges = cv2.bitwise_not(edges)
+cv2.imwrite("palmlines.jpg",edges)
+palmlines = cv2.imread("palmlines.jpg")
+img = cv2.addWeighted(palmlines,0.3,image,0.7,0)
